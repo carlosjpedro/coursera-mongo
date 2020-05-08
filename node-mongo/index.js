ba48f9bf4ff66b1b41de8e079f76075482dcbd0c
Carlos Pedro <carlospedr@gmail.com>
2020-05-08 19:37:13 +0200

    fist

---
+++ node-mongo/index.js
@@ -0,0 +1,39 @@
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
+const dboper = require('./operations')
+const url = 'mongodb://localhost:27017'
+const dbname = 'conFusion'
+
+MongoClient.connect(url, (err, client) => {
+    assert.equal(err, null)
+
+    console.log('Connected to MongoDb')
+    const db = client.db(dbname)
+
+    dboper.insertDocument(db, { name: 'Vadonut', description: 'Test' }, 'dishes')
+        .then(result => {
+            console.log('Insert Document:\n', result.ops)
+            return dboper.findDocuments(db, 'dishes')
+        })
+        .then(
+            doc => {
+                console.log('Found Document:\n', doc)
+                return dboper.updateDocument(db, { name: 'Vadonut' }, { description: 'Updated Test' }, 'dishes')
+
+            })
+        .then((result) => {
+            console.log('Updated Document:\n', result.result)
+            return dboper.findDocuments(db, 'dishes')
+        })
+        .then((doc) => {
+            console.log('Found Document:\n', doc)
+            return db.dropCollection('dishes')
+        })
+        .then(() => {
+            client.close()
+        })
+        .catch(err => console.log(err))
+
+})
+
+
